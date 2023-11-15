fb033e186648e62d35bcf1a35395dd10ce053306
Luis Yerga <lyergamayor1@gmail.com>
2023-11-15 10:20:38 +0100

    carrusel

--- php/index.php
+++ php/index.php
@@ -10,15 +10,15 @@
   <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/custom.css">
-</head> 
+  <link rel="stylesheet" href="../css/custom.css">
+</head>
 
 <body>
-    <header>
-      <?php
-      include("header.php")
+  <header>
+    <?php
+    include("header.php")
       ?>
-    </header>
+  </header>
   <main>
     <section class="introduccion">
       <div class="container" id="contenedor1">
@@ -39,13 +39,14 @@
     </section>
     <section class="sobreMi">
       <div class="separador" id="separador1">
-        <hr>          
+        <hr>
         <h3>Sobre Mi</h3>
       </div>
       <div class="container" id="contenedor2">
         <div id="informacion">
           <p>Hola, mi nombre es Luis Yerga Mayor. Estudie el grado de Desarrollo de Aplicaciones Web.</p>
-          <p>Entre mis capacidades se encuentra el desarrollo en front, aunque personalmente me gusta más dedicarme al backend.</p>
+          <p>Entre mis capacidades se encuentra el desarrollo en front, aunque personalmente me gusta más dedicarme al
+            backend.</p>
         </div>
         <div id="fotoCodigo">
           <img id="imagenPerfil" src="../img/perfil.jpg" alt="Foto pefil">
@@ -72,10 +73,52 @@
     <section class="proyectos">
       <div class="separador" id="separador2">
         <h3>Proyectos</h3>
-        <hr>          
+        <hr>
       </div>
       <div class="container" id="contenedor3">
-
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+              aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+              aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+              aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="..." class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="..." class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="..." class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
     </section>
   </main>
@@ -85,10 +128,11 @@
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
     integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
+    </script>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
+    </script>
 </body>
+
 </html>
